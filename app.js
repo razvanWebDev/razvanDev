c4fca82b1ab7192395f813ce3efd0f88128e0367
Crisan Razvan <crsn_razvan@yahoo.com>
2019-06-06 20:18:52 +0300

    added projects from JSON file

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const header = document.querySelector('header');
 const navLinks = document.querySelectorAll('.menuButtons');
-console.log(navLinks);
 
 // Change header background on scroll
 function headerBackground () {
@@ -16,7 +15,57 @@ function headerBackground () {
 }
 
 
+//Load Projects from JSON
+function loadProjects (){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+       if(this.readyState == 4 && this.status == 200) {
+        //    console.log('response: ' + xhttp.responseText);
+           var projects = JSON.parse(xhttp.responseText);
+           displayProjects(projects);
+       }
+   }
+   xhttp.open('GET', "projects.json", true);
+   xhttp.send();
+}
+
+//DISPLAY PROJECTS
+function displayProjects(projects) {
+    const resultList = document.querySelector('.projects');
+    const listItems = projects.map(function (project) {
+  
+      const name = project.name;
+      const image = project.imgPath;
+      const codeLink = project.codeLink;
+      const demoLink = project.demoLink;
+      const description = project.description;
+      const skillsUsed = project.skillsUsed;
+  
+  
+      return `<div class="project-div">
+                <h3>${name} </h3>
+                <div class="img-container">
+                  <img src = ${image} alt="pic" class="project-pic">
+                  <div class="project-links">
+                                <a href="${codeLink}" target="_blank">
+                                  <button class="btn"> View Code</button>
+                                 </a>
+                                <a href="${demoLink}" target="_blank">
+                                  <button class="btn">View Demo</button>
+                                 </a>
+                            </div>
+                </div>
+                <p>${description}</p>
+                <p>${skillsUsed}</p>
+              </div>`;
+    })
+  
+    resultList.innerHTML = listItems.join('');
+  }
+
 
 // Event listeners
 window.addEventListener("scroll", headerBackground);
 window.addEventListener("load", headerBackground);
+
+loadProjects ();
